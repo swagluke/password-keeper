39e1263e631c9c7388c4d87bbf4ab44c78f187a0
Luke Zhang <zhangl@rose-hulman.edu>
2017-03-23 08:45:25 -0400

    Finished signin flow and bouncing.

--- PasswordKeeper/src/app/+main/main.component.ts
+++ PasswordKeeper/src/app/+main/main.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseAuthState } from 'angularfire2';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
-
-  constructor() { }
+export class MainComponent implements OnInit, OnDestroy {
+  private authSubscription: Subscription;
+  constructor(private af: AngularFire, private router: Router) {
+    this.authSubscription = af.auth.subscribe((auth: FirebaseAuthState) => {
+      if (auth) {
+        console.log("You are signed in. All is good.");
+      } else {
+        console.log("Not sign in. Bounce to the /signin page.");
+        this.router.navigate(['/signin']);
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 }
--- PasswordKeeper/src/app/app.component.ts
+++ PasswordKeeper/src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { AngularFireAuth, FirebaseAuthState } from 'angularfire2';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Subscription } from "rxjs/Subscription";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  showSignOut = true;
+export class AppComponent implements OnDestroy {
+  showSignOut = false;
+  private authSubscription: Subscription;
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.subscribe((auth: FirebaseAuthState) => {
 
-  signOut(): void{
-    console.log("TODO: Sign out");
+      if (auth) {
+        this.showSignOut = true;
+      } else {
+        this.showSignOut = false;
+      }
+    })
+  }
+
+  signOut(): void {
+    this.afAuth.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 }
