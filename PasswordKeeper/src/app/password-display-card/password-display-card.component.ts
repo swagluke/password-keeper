3acdc92de9cd50d0fc52b7ba476fb7f746c1788d
Luke Zhang <zhangl@rose-hulman.edu>
2017-03-28 09:49:02 -0400

    Finished card animation.

--- PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
+++ PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, style, transition, animate } from '@angular/core';
 import { Password } from './../password.model';
 @Component({
   selector: 'password-display-card',
   templateUrl: './password-display-card.component.html',
-  styleUrls: ['../shared/common.scss', './password-display-card.component.scss']
+  styleUrls: ['../shared/common.scss', './password-display-card.component.scss'],
+  animations: [
+    trigger('showPassword', [
+      state('collapsed', style({
+        height: 0
+      })),
+      state('expanded', style({
+
+      })),
+      transition('* => *', animate('0.3s'))
+    ])
+  ]
 })
 export class PasswordDisplayCardComponent implements OnInit {
 
   @Input() password: Password;
+  isExpanded = false;
 
+  get showPasswordState(): string {
+    return this.isExpanded ? "expanded" : "collapsed";
+  }
   constructor() { }
 
   ngOnInit() {
